a0ef466144d977224cad9ca375d2198f8e262326
wyattcolburn <wyattdcolburn@gmail.com>
2025-05-06 14:53:21 -0700

    Applied .gitignore

--- Core/Src/spi.c
+++ Core/Src/spi.c
@@ -4,7 +4,117 @@
 
 
 #include "spi.h"
+void SPI_Slave_Init( void) {
+  RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+  RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOAEN | RCC_AHB2ENR_GPIOEEN);
 
+  GPIOE->MODER &= ~(GPIO_MODER_MODE12 | GPIO_MODER_MODE13 | GPIO_MODER_MODE15);
+  // set MODE4/5/7 to 10 (alternate function)
+  GPIOE->MODER |= (GPIO_MODER_MODE12_1 | GPIO_MODER_MODE13_1 | GPIO_MODER_MODE15_1);
+  // push-pull (0)
+  GPIOE->OTYPER &= ~(GPIO_OTYPER_OT12 | GPIO_OTYPER_OT13 | GPIO_OTYPER_OT15);
+  // low speed (0)
+  GPIOE->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEED12 | GPIO_OSPEEDR_OSPEED13 | GPIO_OSPEEDR_OSPEED15);
+  // no pull up / no pull down (00)
+  GPIOE->PUPDR &= ~(GPIO_PUPDR_PUPD12 | GPIO_PUPDR_PUPD13 | GPIO_PUPDR_PUPD15);
+
+  GPIOE->AFR[1] |= (5 << GPIO_AFRH_AFSEL12_Pos |
+					  5 << GPIO_AFRH_AFSEL13_Pos | //SCL
+					  5 << GPIO_AFRH_AFSEL14_Pos | //MISO
+					  5 << GPIO_AFRH_AFSEL15_Pos); //MOSI
+
+   SPI1->CR1 &= ~( SPI_CR1_SPE );             	// disable SPI for config
+   SPI1->CR1 &= ~( SPI_CR1_RXONLY );          	// recv-only OFF
+   SPI1->CR1 &= ~( SPI_CR1_LSBFIRST );        	// data bit order MSb:LSb
+   SPI1->CR1 &= ~( SPI_CR1_CPOL | SPI_CR1_CPHA ); // SCLK polarity:phase = 0:0
+   SPI1->CR1 &=	 ~(SPI_CR1_MSTR);              	// MCU is SPI Slave
+   // CR2 (reset value = 0x0700 : 8b data)
+   SPI1->CR2 &= ~( SPI_CR2_TXEIE | SPI_CR2_RXNEIE ); // disable FIFO intrpts
+   SPI1->CR2 &= ~( SPI_CR2_FRF);              	// Moto frame format
+   SPI1->CR2 |=	 SPI_CR2_NSSP;              	// auto-generate NSS pulse
+   SPI1->CR2 |=	 SPI_CR2_DS;                	// 16-bit data
+   SPI1->CR2 |=	 SPI_CR2_SSOE;              	// enable SS output
+   // CR1
+   SPI1->CR1 |=	 SPI_CR1_SPE;               	// re-enable SPI for ops
+}
+
+void SPI_Master_Init( void) {
+  RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+  RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOAEN | RCC_AHB2ENR_GPIOEEN);
+
+  GPIOE->MODER &= ~(GPIO_MODER_MODE12 | GPIO_MODER_MODE13 | GPIO_MODER_MODE15);
+  // set MODE4/5/7 to 10 (alternate function)
+  GPIOE->MODER |= (GPIO_MODER_MODE12_1 | GPIO_MODER_MODE13_1 | GPIO_MODER_MODE15_1);
+  // push-pull (0)
+  GPIOE->OTYPER &= ~(GPIO_OTYPER_OT12 | GPIO_OTYPER_OT13 | GPIO_OTYPER_OT15);
+  // low speed (0)
+  GPIOE->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEED12 | GPIO_OSPEEDR_OSPEED13 | GPIO_OSPEEDR_OSPEED15);
+  // no pull up / no pull down (00)
+  GPIOE->PUPDR &= ~(GPIO_PUPDR_PUPD12 | GPIO_PUPDR_PUPD13 | GPIO_PUPDR_PUPD15);
+
+  GPIOE->AFR[1] |= (5 << GPIO_AFRH_AFSEL12_Pos |
+					  5 << GPIO_AFRH_AFSEL13_Pos | //SCL
+					  5 << GPIO_AFRH_AFSEL15_Pos); //MOSI
+
+   SPI1->CR1 &= ~( SPI_CR1_SPE );             	// disable SPI for config
+   SPI1->CR1 &= ~( SPI_CR1_RXONLY );          	// recv-only OFF
+   SPI1->CR1 &= ~( SPI_CR1_LSBFIRST );        	// data bit order MSb:LSb
+   SPI1->CR1 &= ~( SPI_CR1_CPOL | SPI_CR1_CPHA ); // SCLK polarity:phase = 0:0
+   SPI1->CR1 &=	 ~(SPI_CR1_MSTR);              	// MCU is SPI Slave
+   // CR2 (reset value = 0x0700 : 8b data)
+   SPI1->CR2 &= ~( SPI_CR2_TXEIE | SPI_CR2_RXNEIE ); // disable FIFO intrpts
+   SPI1->CR2 &= ~( SPI_CR2_FRF);              	// Moto frame format
+   SPI1->CR2 |=	 SPI_CR2_NSSP;              	// auto-generate NSS pulse
+   SPI1->CR2 |=	 SPI_CR2_DS;                	// 16-bit data
+   SPI1->CR2 |=	 SPI_CR2_SSOE;              	// enable SS output
+   // CR1
+   SPI1->CR1 |=	 SPI_CR1_SPE;               	// re-enable SPI for ops
+}
+
+
+void SPI_Master_Init( void) {
+
+  //SPI 2 uses PD0 for NSS, PD1 fo r SCL, PD3 for MISO, PD4 for MOSI
+  RCC->APB2ENR |= RCC_APB2ENR_SPI2EN;
+  RCC->AHB2ENR |= (RCC_AHB2ENR_GPIODEN);
+
+  GPIOS->MODER &= ~(GPIO_MODER_MODE12 | GPIO_MODER_MODE13 | GPIO_MODER_MODE15);
+  // set MODE4/5/7 to 10 (alternate function)
+  GPIOD->MODER |= (GPIO_MODER_MODE12_1 | GPIO_MODER_MODE13_1 | GPIO_MODER_MODE15_1);
+  // push-pull (0)
+  GPIOD->OTYPER &= ~(GPIO_OTYPER_OT12 | GPIO_OTYPER_OT13 | GPIO_OTYPER_OT15);
+  // low speed (0)
+  GPIOD->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEED12 | GPIO_OSPEEDR_OSPEED13 | GPIO_OSPEEDR_OSPEED15);
+  // no pull up / no pull down (00)
+  GPIOD->PUPDR &= ~(GPIO_PUPDR_PUPD12 | GPIO_PUPDR_PUPD13 | GPIO_PUPDR_PUPD15);
+
+  GPIOD->AFR[1] |= (5 << GPIO_AFRH_AFSEL12_Pos |
+					  5 << GPIO_AFRH_AFSEL13_Pos | //SCL
+					  5 << GPIO_AFRH_AFSEL14_Pos | //MISO
+					  5 << GPIO_AFRH_AFSEL15_Pos); //MOSI
+
+   SPI2->CR1 &= ~( SPI_CR1_SPE );             	// disable SPI for config
+   SPI2->CR1 &= ~( SPI_CR1_RXONLY );          	// recv-only OFF
+   SPI2->CR1 &= ~( SPI_CR1_LSBFIRST );        	// data bit order MSb:LSb
+   SPI2->CR1 &= ~( SPI_CR1_CPOL | SPI_CR1_CPHA ); // SCLK polarity:phase = 0:0
+   SPI2->CR1 &=	 ~(SPI_CR1_MSTR);              	// MCU is SPI Slave
+   // CR2 (reset value = 0x0700 : 8b data)
+   SPI2->CR2 &= ~( SPI_CR2_TXEIE | SPI_CR2_RXNEIE ); // disable FIFO intrpts
+   SPI2->CR2 &= ~( SPI_CR2_FRF);              	// Moto frame format
+   SPI2->CR2 |=	 SPI_CR2_NSSP;              	// auto-generate NSS pulse
+   SPI2->CR2 |=	 SPI_CR2_DS;                	// 16-bit data
+   SPI2->CR2 |=	 SPI_CR2_SSOE;              	// enable SS output
+   // CR1
+   SPI2->CR1 |=	 SPI_CR1_SPE;               	// re-enable SPI for ops
+}
+
+uint16_t SPI_Read_From_Peer() {
+    // Wait until receive buffer contains data
+    while(!(SPI1->SR & SPI_SR_RXNE));
+
+    // Return received data
+    return SPI1->DR;
+}
 void SPI_init( void ) {
    // SPI config as specified @ STM32L4 RM0351 rev.9 p.1459
    // called by or with DAC_init()
